c63dda4a2e9d24d94cd13c85664e1b7490265cf8
AhmedRaza-Bakker <ahmedraza27058@gmail.com>
2022-03-27 11:08:58 +0500

    r

--- ClothBazar/ClothBazar.Web/Models/AccountViewModels.cs
+++ ClothBazar/ClothBazar.Web/Models/AccountViewModels.cs
@@ -7,7 +7,7 @@ namespace ClothBazar.Web.Models
     {
         [Required]
         [Display(Name = "Email")]
-        public string Email { get; set; } V
+        public string Email { get; set; }  
     }
 
     public class ExternalLoginListViewModel
